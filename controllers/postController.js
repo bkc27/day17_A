09b84e2a16da12f744052f9983cb7232c56c47c9
unknown <bkc.vns@gmail.com>
2026-06-30 10:36:29 +0530

    Post Crud

--- controllers/postController.js
+++ controllers/postController.js
@@ -46,9 +46,52 @@ const getAllPost = async (req, res) => {
     }
 };
 
-const getMyPost = () => { 
-    
+const getMyPost = async (req, res) => {
+    try {
+        const posts = await Post.find({ user: req.user._id });
+        res.status(201).json({
+            success: true,
+            message: "Your Posts",
+            total: posts.length,
+            posts
+        });
+    }
+    catch (err) {
+        res.status(500).json({
+            success: false,
+            message: "Unable to Fetch Your Posts",
+            error: err.message
+        })
+    }
 };
+
+
+const getSinglePost = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const post = await Post.findById(id);
+        if(!post){
+            return res.status(401).json({
+                success:false,
+                message:"Post Not Found"
+            });
+        }
+        res.status(201).json({
+            success: true,
+            message: "Post Found",
+            post
+        });
+    }
+    catch (err) {
+        res.status(500).json({
+            success: false,
+            message: "Unable to Fetch Your Posts",
+            error: err.message
+        })
+    }
+};
+
+
 
 // get - all post
 // get - my post
