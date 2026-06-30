60332d41e9945a6102e43b0f66d1448cc2e84368
unknown <bkc.vns@gmail.com>
2026-06-30 09:22:04 +0530

    First

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const mongoose = require("mongoose")
+require("dotenv").config();
+
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(()=>console.log("DB Connected Successfully"))
+.catch((err)=>{console.log("Unable to connect DB ",err)});
+
+app.use("/api/auth",authRoutes);
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT,()=>{
+    console.log("Server Started at "+PORT);
+})
